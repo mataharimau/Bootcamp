579c1e2c6494766f71b2a42817553ef631d4b4fe
mataharimau <audrey.tam@gmail.com>
2020-05-21 09:38:41 +1000

    initial

---
+++ Week01/Week01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Week01
+//
+//  Created by Audrey Tam on 21/5/20.
+//  Copyright © 2020 Razeware. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
